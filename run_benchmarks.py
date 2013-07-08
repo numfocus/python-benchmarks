f3729787302b067d25e277aeb83f9949777a9ba0
Olivier Grisel <olivier.grisel@ensta.org>
2013-07-09 00:14:35 +0200

    New template based report page

--- run_benchmarks.py
+++ run_benchmarks.py
@@ -11,42 +11,92 @@ from jinja2 import Template
 
 log = logging.getLogger(__name__)
 
+GROUP_URL_PATTERN = ("https://github.com/numfocus/python-benchmarks/"
+                     "tree/master/%s")
 
-MAIN_REPORT_TEMPLATE = """
-Benchmark Results
-=================
+ABOUT_URL = ("https://github.com/numfocus/python-benchmarks/blob/master/"
+             "README.md#motivation")
+
+GITHUB_REPO_URL = "https://github.com/numfocus/python-benchmarks"
+
+
+MAIN_REPORT_TEMPLATE = """\
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+</head>
+<body>
+<div class="container">
+
+<ul class="nav nav-pills">
+  <li><a href="{{ github_repo_url }}">Github repo</a></li>
+  <li><a href="{{ about_url }}">About</a></li>
+</ul>
+
+<h2>Python Benchmark Results</h2>
 
 {% for result in bench_results %}
 
-{{ result.group_name }}
------------------------
+<h3><a href="{{ result.source_url }}" title="View source">
+    {{ result.group_name }}</a></h3>
 
-TODO: add link to the sources code on github
+<table class="table">
+<thead>
+<tr>
+    <th>Function name</th>
+    <th>Cold time (s)</th>
+    <th>Warm time (s)</th>
+    <th>Runtime Error</th>
+</tr>
+</thead>
+<tbody>
+{% for record in result.records %}
+<tr>
+    <td><i class="icon-tasks"></i> {{ record.name }}</td>
+    <td>{% if record.cold_time %}
+        {{ "{:0.3f}".format(record.cold_time) }}
+        {% else %}
+        N/A
+        {% endif %}
+    </td>
+    <td>{% if record.warm_time %}
+        {{ "{:0.3f}".format(record.warm_time) }}
+        {% else %}
+        N/A
+        {% endif %}
+    </td>
+    <td>{% if record.runtime_error %}
+        {{ record.runtime_error }}
+        {% else %}
+        N/A
+        {% endif %}
+    </td>
+</tr>
+{% endfor %}
+</tbody>
+</table>
 
-===================== ============= ============= ===============
-Function name         Cold time (s) Warm time (s) Error
-===================== ============= ============= ===============
-
-{% for record in records %}
-
-
+<p>TODO: log import error summary here</p>
 
 {% endfor %}
 
+<h2>Runtime Environment</h2>
 
-Runtime Environment
-===================
-
-Hardware
---------
+<h3>Hardware</h3>
 
 TODO: describe host machine (CPU, RAM, ...) with psutil
 
-Software
---------
+<h3>Software</h3>
+
 
 TODO: list version numbers for all the libraries and compilers.
 
+</div>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
 """
 
 
@@ -112,15 +162,12 @@ def run_benchmarks(folders=None, platforms=None, catch_errors=True,
     bench_results = []
 
     for group_name, make_env, benchmarks, import_errors in collected:
-        logging.info("Running benchmark group %s", group_name)
+        log.info("Running benchmark group %s", group_name)
         args, kwargs = make_env() if make_env is not None else {}
-        print(group_name)
-        print('-' * len(group_name) + '\n')
-        print("Benchmarks:")
 
         records = []
         for name, func in benchmarks:
-            logging.info("Benchmarking %s", name)
+            log.info("Benchmarking %s", name)
             try:
                 cold_time = run_benchmark(func, args, kwargs, memory)
                 warm_time = run_benchmark(func, args, kwargs, memory)
@@ -131,21 +178,24 @@ def run_benchmarks(folders=None, platforms=None, catch_errors=True,
                     runtime_error = e
                 else:
                     raise
-            records.append(OrderedDict(
-                name=name,
-                source_url=None,  # TODO
-                cold_time=cold_time,
-                warm_time=warm_time,
-                runtime_error=runtime_error))
+
+            record = OrderedDict([
+                ('name', name),
+                ('cold_time', cold_time),
+                ('warm_time', warm_time),
+                ('runtime_error', runtime_error),
+            ])
+            records.append(record)
+            log.info(", ".join("%s: %r" % (k, v) for k, v in record.items()))
+
             # TODO: add special support for PyPy with a sub-process
 
-        bench_results.append(OrderedDict(
-            group_name=group_name,
-            source_url=None,  # TODO
-            records=records,
-            import_errors=import_errors,
-        ))
-
+        bench_results.append(OrderedDict([
+            ('group_name', group_name),
+            ('source_url', GROUP_URL_PATTERN % group_name),
+            ('records', records),
+            ('import_errors', import_errors),
+        ]))
     return bench_results
 
 
@@ -153,7 +203,13 @@ if __name__ == "__main__":
     # TODO: use argparse
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
     bench_results = run_benchmarks(catch_errors=True, memory=True)
-    print(Template(MAIN_REPORT_TEMPLATE).render(
+    rendered = Template(MAIN_REPORT_TEMPLATE).render(
         bench_results=bench_results,
         runtime_environment=None,
-    ))
+        about_url=ABOUT_URL,
+        github_repo_url=GITHUB_REPO_URL,
+    )
+    report_filename = 'report/index.html'
+    log.info("Writing report to: %s", report_filename)
+    with open(report_filename, 'wb') as f:
+        f.write(rendered)
