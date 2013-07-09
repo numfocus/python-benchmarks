4531354d2b7626695b7d1ca7fe09a4b6b3257b51
Olivier Grisel <olivier.grisel@ensta.org>
2013-07-09 13:32:02 +0200

    Move the template into a dedicated folder

--- run_benchmarks.py
+++ run_benchmarks.py
@@ -25,139 +25,7 @@ ABOUT_URL = ("https://github.com/numfocus/python-benchmarks/blob/master/"
 GITHUB_REPO_URL = "https://github.com/numfocus/python-benchmarks"
 
 
-MAIN_REPORT_TEMPLATE = """\
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-  <link href="css/custom.css" rel="stylesheet" media="screen">
-</head>
-<body>
-<div class="container">
-
-<ul class="nav nav-pills">
-  <li><a href="{{ github_repo_url }}">Github repo</a></li>
-  <li><a href="{{ json_data_url }}">JSON data</a></li>
-  <li><a href="{{ about_url }}">About</a></li>
-</ul>
-
-<h2>Python Benchmark Results</h2>
-
-{% for result in bench_results %}
-
-<section id="results-{{ result.group_name }}">
-<h3>{{ result.group_name }}
-    (<a href="{{ result.source_url }}"
-     title="Source code for {{ result.group_name }}">source code</a>)
-    <a title="Permalink to this headline"
-       href="#results-{{ result.group_name }}"
-       class="headerlink">&para;</a>
-</h3>
-
-<table class="table table-striped table-hover">
-<colgroup>
-  <col class="bench_icon">
-  <col class="bench_name">
-  <col class="bench_time">
-  <col class="bench_time">
-</colgroup>
-<thead>
-<tr>
-  <th></th>
-  <th>Function name</th>
-  <th>Cold time (s)</th>
-  <th>Warm time (s)</th>
-</tr>
-</thead>
-<tbody>
-{% for record in result.records %}
-<tr>
-  <td><i class="icon-tasks"></i></td>
-  <td>{{ record.name }}</td>
-  <td>{% if record.cold_time %}
-    {{ "{:0.3f}".format(record.cold_time) }}
-    {% else %}
-    N/A
-    {% endif %}
-  </td>
-  <td>{% if record.warm_time %}
-    {{ "{:0.3f}".format(record.warm_time) }}
-    {% else %}
-    N/A
-    {% endif %}
-  </td>
-</tr>
-{% endfor %}
-</tbody>
-</table>
-</section>
-{% endfor %}
-
-<h2>Runtime Environment</h2>
-
-<h3>Hardware</h3>
-
-TODO: describe host machine (CPU, RAM, ...) with psutil
-
-<h3>Software</h3>
-
-TODO: list version numbers for all the libraries and compilers.
-
-<h2>Errors</h2>
-
-{% for result in bench_results %}
-{%if result.import_errors or result.runtime_errors %}
-
-<section id="errors-{{ result.group_name }}">
-<h3>{{ result.group_name }}
-    <a title="Permalink to this headline"
-       href="#errors-{{ result.group_name }}"
-       class="headerlink">&para;</a></h3>
-
-{%if result.import_errors  %}
-<div id="import-errors-{{ result.group_name }}">
-<h4>Benchmark loading errors
-    <a title="Permalink to this headline"
-       href="#import-errors-{{ result.group_name }}"
-       class="headerlink">&para;</a></h4>
-<dl>
-  {% for import_error in result.import_errors %}
-    <dt>{{ import_error.name }}: {{ import_error.error_type }}</dt>
-    <dd>
-      <pre class="pre-scrollable">{{ import_error.traceback }}</pre>
-    </dd>
-  {% endfor %}
-</dl>
-</div>
-{% endif %}
-
-{%if result.runtime_errors  %}
-<div id="runtime-errors-{{ result.group_name }}">
-<h4>Benchmark execution errors
-    <a title="Permalink to this headline"
-       href="#runtime-errors-{{ result.group_name }}"
-       class="headerlink">&para;</a></h4>
-<dl>
-  {% for runtime_error in result.runtime_errors %}
-    <dt>{{ runtime_error.name }}: {{ runtime_error.error_type }}</dt>
-    <dd>
-      <pre class="pre-scrollable">{{ runtime_error.traceback }}</pre>
-    </dd>
-  {% endfor %}
-</dl>
-</div>
-{% endif %}
-
-</section>
-{% endif %}
-{% endfor %}
-
-</div>
-<script src="js/bootstrap.min.js"></script>
-</body>
-</html>
-"""
+MAIN_REPORT_TEMPLATE_FILENAME = "templates/index.html"
 
 
 def find_benchmarks(folders=None, platforms=None):
@@ -280,13 +148,14 @@ def run_benchmarks(folders=None, platforms=None, catch_errors=True,
 
 
 def build_report(report_filename=REPORT_FILENAME, data_filename=DATA_FILENAME):
-    rendered = Template(MAIN_REPORT_TEMPLATE).render(
-        bench_results=bench_results,
-        runtime_environment=None,
-        about_url=ABOUT_URL,
-        github_repo_url=GITHUB_REPO_URL,
-        json_data_url=os.path.basename(bench_data_filename),
-    )
+    with open(MAIN_REPORT_TEMPLATE_FILENAME, 'rb') as f:
+        rendered = Template(f.read()).render(
+            bench_results=bench_results,
+            runtime_environment=None,
+            about_url=ABOUT_URL,
+            github_repo_url=GITHUB_REPO_URL,
+            json_data_url=os.path.basename(bench_data_filename),
+        )
     report_filename = 'report/index.html'
     log.info("Writing report to: %s", report_filename)
     with open(report_filename, 'wb') as f:
