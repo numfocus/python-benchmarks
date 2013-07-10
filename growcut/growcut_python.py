d2d6d71d9ae2802587bc8ee6f27cddceca5edc30
Olivier Grisel <olivier.grisel@ensta.org>
2013-07-10 19:07:38 +0200

    More exact authors list, remove continue in inner loop and make it easy to switch the impl of sqrt

--- growcut/growcut_python.py
+++ growcut/growcut_python.py
@@ -1,6 +1,6 @@
-# Authors: Continuum Analytics, Olivier Grisel
+# Authors: Stefan van der Walt, Aron Ahmadia, Olivier Grisel
 
-import math
+from math import sqrt
 
 
 def window_floor(idx, radius):
@@ -19,7 +19,7 @@ def window_ceil(idx, ceil, radius):
 
 def growcut_python(image, state, state_next, window_radius):
     changes = 0
-    sqrt_3 = math.sqrt(3.0)
+    sqrt_3 = sqrt(3.0)
 
     height = image.shape[0]
     width = image.shape[1]
@@ -34,22 +34,20 @@ def growcut_python(image, state, state_next, window_radius):
                              window_ceil(j + 1, width, window_radius)):
                 for ii in xrange(window_floor(i, window_radius),
                                  window_ceil(i + 1, height, window_radius)):
-                    if (ii == i and jj == j):
-                        continue
+                    if ii != i or jj != j:
+                        d = image[i, j, 0] - image[ii, jj, 0]
+                        s = d * d
+                        for k in range(1, 3):
+                            d = image[i, j, k] - image[ii, jj, k]
+                            s += d * d
+                        gval = 1.0 - sqrt(s) / sqrt_3
 
-                    d = image[i, j, 0] - image[ii, jj, 0]
-                    s = d * d
-                    for k in range(1, 3):
-                        d = image[i, j, k] - image[ii, jj, k]
-                        s += d * d
-                    gval = 1.0 - math.sqrt(s) / sqrt_3
+                        attack_strength = gval * state[ii, jj, 1]
 
-                    attack_strength = gval * state[ii, jj, 1]
-
-                    if attack_strength > defense_strength:
-                        defense_strength = attack_strength
-                        winning_colony = state[ii, jj, 0]
-                        changes += 1
+                        if attack_strength > defense_strength:
+                            defense_strength = attack_strength
+                            winning_colony = state[ii, jj, 0]
+                            changes += 1
 
             state_next[i, j, 0] = winning_colony
             state_next[i, j, 1] = defense_strength
