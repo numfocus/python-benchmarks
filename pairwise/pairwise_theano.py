ef048f111089be24efe8a322826cfbe2f042c975
Olivier Grisel <olivier.grisel@ensta.org>
2013-07-10 22:09:12 +0200

    Add creditis + pep8

--- pairwise/pairwise_theano.py
+++ pairwise/pairwise_theano.py
@@ -1,8 +1,8 @@
-
+# Authors: James Bergstra
+# License: MIT
 import theano
 import theano.tensor as TT
 
-from pairwise_python import pairwise_python_inner_broadcasting
 
 def pairwise_theano_tensor_prepare(dtype):
     X = TT.matrix(dtype=str(dtype))
@@ -11,18 +11,19 @@ def pairwise_theano_tensor_prepare(dtype):
             TT.sqr(X[:, None, :] - X),
             axis=2))
     rval = theano.function([X],
-            theano.Out(dists, borrow=True),
-            allow_input_downcast=True)
+                           theano.Out(dists, borrow=True),
+                           allow_input_downcast=True)
     rval.__name__ = 'pairwise_theano_tensor_' + dtype
     return rval
+
 
 def pairwise_theano_blas_prepare(dtype):
     X = TT.matrix(dtype=str(dtype))
     X_norm_2 = (X ** 2).sum(axis=1)
     dists = TT.sqrt(2 * X_norm_2 - TT.dot(X, X.T))
     rval = theano.function([X],
-            theano.Out(dists, borrow=True),
-            allow_input_downcast=True)
+                           theano.Out(dists, borrow=True),
+                           allow_input_downcast=True)
     rval.__name__ = 'pairwise_theano_blas_' + dtype
     return rval
 
@@ -33,5 +34,3 @@ benchmarks = (
     pairwise_theano_blas_prepare('float32'),
     pairwise_theano_blas_prepare('float64'),
 )
-
-
