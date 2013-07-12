0645184904079115265f8ad445f73e888c6946b3
Olivier Grisel <olivier.grisel@ensta.org>
2013-07-12 10:18:39 +0200

    PEP8

--- rosen_der/rosen_der_python.py
+++ rosen_der/rosen_der_python.py
@@ -9,9 +9,11 @@ def rosen_der_numpy(x):
     xm_m1 = x[:-2]
     xm_p1 = x[2:]
     der = numpy.zeros_like(x)
-    der[1:-1] = 200*(xm-xm_m1**2) - 400*(xm_p1 - xm**2)*xm - 2*(1-xm)
-    der[0] = -400*x[0]*(x[1]-x[0]**2) - 2*(1-x[0])
-    der[-1] = 200*(x[-1]-x[-2]**2)
+    der[1:-1] = (+ 200 * (xm - xm_m1 ** 2)
+                 - 400 * (xm_p1 - xm ** 2) * xm
+                 - 2 * (1 - xm))
+    der[0] = -400 * x[0] * (x[1] - x[0] ** 2) - 2 * (1 - x[0])
+    der[-1] = 200 * (x[-1] - x[-2] ** 2)
     return der
 
 
@@ -19,13 +21,13 @@ def rosen_der_python(x):
     n = x.shape[0]
     der = numpy.zeros_like(x)
 
-    for i in range(1, n-1):
-        der[i] = (+ 200*(x[i]-x[i-1]**2)
-                  - 400*(x[i+1]
-                  - x[i]**2)*x[i]
-                  - 2*(1-x[i]))
-    der[0] = -400*x[0]*(x[1]-x[0]**2) - 2*(1-x[0])
-    der[-1] = 200*(x[-1]-x[-2]**2)
+    for i in range(1, n - 1):
+        der[i] = (+ 200 * (x[i] - x[i - 1] ** 2)
+                  - 400 * (x[i + 1]
+                           - x[i] ** 2) * x[i]
+                  - 2 * (1 - x[i]))
+    der[0] = -400 * x[0] * (x[1] - x[0] ** 2) - 2 * (1 - x[0])
+    der[-1] = 200 * (x[-1] - x[-2] ** 2)
     return der
 
 benchmarks = (rosen_der_numpy, rosen_der_python)
