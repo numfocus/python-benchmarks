90ebb7c2b22e6a878aa513b554069849d93dbc50
James Bergstra <james.bergstra@gmail.com>
2013-07-15 14:29:29 -0400

    use matrix symmetry in pairwise pyopencl

--- pairwise/pairwise_pyopencl.py
+++ pairwise/pairwise_pyopencl.py
@@ -34,12 +34,13 @@ def pairwise_pyopencl_cpu_prepare(shp, dtype):
     """
 
     prg = cl.Program(ctx, """
-        __kernel void foo(__global %(ctype)s2 *a, __global %(ctype)s *c)
+        __kernel void lower(__global %(ctype)s2 *a, __global %(ctype)s *c)
         {
           for(int n0 = get_global_id(0); n0 < %(N)s; n0 += get_global_size(0))
           {
               for(int m0 = get_global_id(1); m0 < %(N)s; m0 += get_global_size(1))
               {
+                if (n0 < m0) continue;
                   __global %(ctype)s2 *an = a + n0 * %(D)s / 2;
                   __global %(ctype)s2 *am = a + m0 * %(D)s / 2;
                   %(ctype)s2 buf = 0;
@@ -53,27 +54,46 @@ def pairwise_pyopencl_cpu_prepare(shp, dtype):
               }
           }
         }
+        __kernel void upper(__global %(ctype)s *a, __global %(ctype)s *c)
+        {
+          for(int n0 = get_global_id(0); n0 < %(N)s; n0 += get_global_size(0))
+          {
+              for(int m0 = get_global_id(1); m0 < %(N)s; m0 += get_global_size(1))
+              {
+                if (n0 >= m0) continue;
+                  c[m0 * %(N)s + n0] = c[n0 * %(N)s + m0];
+              }
+          }
+        }
         """ % locals()).build()
 
-    return prg.foo
+    return prg.lower, prg.upper
 
 
 comptimes = []
 def pairwise_pyopencl_cpu(data):
     data = np.asarray(data, order='C')
     N, D = data.shape
-    rval = np.empty((N, N), dtype=data.dtype)
     try:
-        f = _cache[(data.shape, data.dtype)]
+        lower, upper = _cache[(data.shape, data.dtype)]
     except:
-        f = pairwise_pyopencl_cpu_prepare(data.shape, data.dtype)
-        _cache[(data.shape, data.dtype)] = f
+        lower, upper = pairwise_pyopencl_cpu_prepare(data.shape, data.dtype)
+        _cache[(data.shape, data.dtype)] = lower, upper
     data_buf = cl.Buffer(ctx, mf.COPY_HOST_PTR, hostbuf=data)
     dest_buf = cl.Buffer(ctx, mf.WRITE_ONLY, N * N * data.dtype.itemsize)
-    rval, _ = cl.enqueue_map_buffer(queue, dest_buf, cl.map_flags.READ,
-            offset=0, shape=(N, N), dtype=data.dtype)
-    ev = f(queue, (N, 1), (1, 1), data_buf, dest_buf)
-    queue.finish()
+    try:
+        rval, _ = cl.enqueue_map_buffer(queue, dest_buf, cl.map_flags.READ,
+                offset=0, shape=(N, N), dtype=data.dtype)
+        need_copy = False
+    except TypeError: #OSX's OCL needs this?
+        rval = np.empty((N, N), dtype=data.dtype)
+        need_copy = True
+    lower(queue, (N, 1), (1, 1), data_buf, dest_buf)
+    upper(queue, (4, 4), (1, 1), data_buf, dest_buf)
+    if need_copy:
+        cl.enqueue_copy(queue, rval, dest_buffer)
+    else:
+        queue.finish()
     if PROFILING:
         comptimes.append(1e-9 * (ev.profile.end - ev.profile.start))
         print 'computation time', min(comptimes)
