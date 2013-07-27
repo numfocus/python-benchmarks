d7686d7b167efff48e7b6a4c2cd17cf09e76f9a7
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2013-07-27 09:47:38 -0600

    Create arc_distance_parakeet.py

---
+++ arc_distance/arc_distance_parakeet.py
@@ -0,0 +1,10 @@
+# Authors: Yuancheng Peng
+# License: MIT
+
+from arc_distance import arc_distance_python as adp
+from parakeet import jit
+
+
+benchmarks = (("arc_distance_parakeet_for_loops",
+               jit(adp.arc_distance_python_nested_for_loops)),
+              )
