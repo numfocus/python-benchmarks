67d87165c231327700900a89492d145ced9dacf2
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2013-10-13 21:21:31 -0400

    added explanation of commented out julia code in Parakeet

--- julia/julia_parakeet.py
+++ julia/julia_parakeet.py
@@ -4,6 +4,12 @@ from parakeet import jit
 benchmarks = (
     ("julia_parakeet_for_loops",
      jit(julia_python.julia_python_for_loops)),
+
+    # Can't run the NumPy version under Parakeet since the following
+    # features are not supported: 
+    #   - np.seterr 
+    #   - np.ogrid
+    #   - complex numbers 
     #("julia_parakeet_numpy",
     # jit(julia_python.julia_python_numpy)),
 )
